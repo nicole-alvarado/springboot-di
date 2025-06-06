42365d492e3cbaee9fb4ccd9240bd2790fc39ae1
Nicole <alvaradonicole123@hotmail.com>
2025-06-06 17:58:44 -0300

    Agregar métodos findAll y findById de productos

---
+++ src/main/java/com/nicole/springboot/di/app/springboot_di/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.nicole.springboot.di.app.springboot_di.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.nicole.springboot.di.app.springboot_di.models.Product;
+import com.nicole.springboot.di.app.springboot_di.repositories.ProductRepository;
+
+public class ProductService {
+
+    private ProductRepository repository = new ProductRepository();
+
+    public List<Product> findAll(){
+        
+        return repository.findAll().stream().map(p -> {
+            Double priceImp = p.getPrice() * 1.25d;
+            p.setPrice(priceImp.longValue());
+            return p;
+        }).collect(Collectors.toList());
+    }
+
+    public Product findById(Long id){
+        return repository.findById(id);
+    }
+}
